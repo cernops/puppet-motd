4bf7bdc507f269f1b046fdb02a933c75d131e4d9
Trey Dockendorf <treydock@gmail.com>
2014-10-06 22:47:07 -0500

    Replace hiera_array function call with hiera as the hiera_array call removes duplicate elements which can have unintended results

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,10 +2,12 @@
 class motd::params {
 
   # Specify an array of entries to appear at the top the /etc/motd file allways.
-  $default_header = ["Welcome to ${::fqdn}, ${::operatingsystem}, ${::operatingsystemrelease}",
+  $default_header = [
+    "Welcome to ${::fqdn}, ${::operatingsystem}, ${::operatingsystemrelease}",
     'Archive of news is available in /etc/motd-archive'
-    ]
-  $motd_header    = hiera_array('motd_header',$default_header)
+  ]
+
+  $motd_header    = hiera('motd_header',$default_header)
 
   # The delimeter between the sections of the /etc/motd file can be specified.
   $default_delimiter = '********************************************************************'
